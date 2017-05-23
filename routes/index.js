4f6698aa867a95bd860dc2d32faf744585afd02e
Orlando J Betancourth <orlando.betancourth@gmail.com>
2017-05-22 19:13:48 -0600

    Ruta About

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Image Scrabble Game' });
 });
+
+router.get('/about', function(req, res, next){
+  res.render('about', {"msg":"Hola Mundo!!!"});
+}
+);
 
 module.exports = router;
